4a72a558997cbdd679f5a889096c42b847dad33d
Bruno Henrique <brunohenrique242@gmail.com>
2024-07-16 14:36:13 -0300

    Adicionando produtos ao feed

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,11 +1,33 @@
 import Head from "../../../components/Head";
+import Snacks from "../../../components/Snacks";
 import SnackTitle from "../../../components/SnackTitle";
 
 const Burgers = () => {
+  const data = [
+    {
+      id: 1,
+      snack: "burger",
+      name: "Mega",
+      description:
+        "O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon",
+      price: 25.5,
+      image: "https://i.imgur.com/upjIUnG.jpeg",
+    },
+    {
+      id: 2,
+      snack: "burger",
+      name: "Extra bacon",
+      description: "O artesanal para os amantes de bacon",
+      price: 23.5,
+      image: "https://i.imgur.com/BsxjAyR.jpeg",
+    },
+  ];
+
   return (
     <>
       <Head title="Hambúrgueres" description="Nossos melhores burgues" />
       <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={data} />
     </>
   );
 };
