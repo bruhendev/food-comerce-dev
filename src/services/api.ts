56f420c1b5229e2ca9ebb481412058f3c24d7268
Bruno Henrique <brunohenrique242@gmail.com>
2024-07-17 15:08:37 -0300

    Instalando json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,9 @@ const baseURL = import.meta.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({ baseURL: baseURL });
 
+export const getBurgers = () => api.get("/burguers");
+export const getPizzas = () => api.get("/pizzas");
+export const getDrinks = () => api.get("/drinks");
+export const getIceCreams = () => api.get("/ice-creams");
+
 export default api;
